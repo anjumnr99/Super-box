df228c66bf68d133740d4589f2545a9d78e9fbc5
marizal-akib <marizalakib@gmail.com>
2024-06-19 01:40:40 +0100

    fixed tailwindcss i problem

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -4,6 +4,7 @@ const LandingPage = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <p className="text-4xl font-bold text-blue-500">Hello</p>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import './index.css'
-
 import {
   RouterProvider,
 } from "react-router-dom";
-
+import "./index.css";
 import mainRouter from './Routers/mainRouter';
 
 
